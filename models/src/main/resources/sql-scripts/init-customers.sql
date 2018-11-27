9c2106f5d5cf2e34f728500768be3d41ce58512f
db6692 <db6692@student.uni-lj.si>
2018-11-27 10:40:40 +0100

    init3

--- models/src/main/resources/sql-scripts/init-customers.sql
+++ models/src/main/resources/sql-scripts/init-customers.sql
@@ -1,3 +1,3 @@
 INSERT INTO catalogue (picture, idProfila, opis) VALUES ('/9j/4AAQSkZJRgABAQEAYABgAAD', '1', 'Sea Sunset');
 INSERT INTO catalogue (picture, idProfila, opis) VALUES ('/9j/4AAQSkZJRgABAQEASABIAAD', '2', 'Flower');
-INSERT INTO catalogue (picture, idProfila, opis) VALUES ('/9j/4AAQSkZJRgABAQAAAQABAAD/2w', '2', 'Autumn Waterfall');
+INSERT INTO catalogue (picture, idProfila, opis) VALUES ('/9j/4AAQSkZJRgABAQAAAQABAAD/2w', '3', 'Autumn Waterfall');

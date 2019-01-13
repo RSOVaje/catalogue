059f3da339d111f572c755352d0a679d6a2d0a0b
db6692 <db6692@student.uni-lj.si>
2019-01-13 21:34:32 +0100

    version 0.3

--- models/src/main/resources/sql-scripts/init-customers.sql
+++ models/src/main/resources/sql-scripts/init-customers.sql
@@ -3,6 +3,6 @@ INSERT INTO cataloguePictures (idpicture, idcatalogue) VALUES ('2', '2');
 INSERT INTO cataloguePictures (idpicture, idcatalogue) VALUES ('3', '3');
 INSERT INTO cataloguePictures (idpicture, idcatalogue) VALUES ('1', '3');
 
-INSERT INTO catalogue (id, idprofila, opis) VALUES ('1', '1', 'Summer');
-INSERT INTO catalogue (id, idprofila, opis) VALUES ('2', '2', 'Flowers');
-INSERT INTO catalogue (id, idprofila, opis) VALUES ('2', '3', 'Autumn');
+INSERT INTO catalogue (idprofila, opis) VALUES ('1', '1', 'Summer');
+INSERT INTO catalogue (idprofila, opis) VALUES ('2', '2', 'Flowers');
+INSERT INTO catalogue (idprofila, opis) VALUES ('2', '3', 'Autumn');
